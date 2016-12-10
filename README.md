06721a3e90ec68cb55290cfd16f889d470bab101
kzotoff <k.zotov@gmail.com>
2016-12-10 14:20:02 +0300

    deleted all old files

--- README.md
+++
@@ -1,2 +0,0 @@
-# JuliaCMS
-Lightweight CMS
--- tinymce/plugins/minigallery/_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('DB_PATH', 'db.sqlite');
-define('DB_PREFIX', 'minigallery');
-define('RELATIVE_DIR', '../../../userfiles/images/');
-define('ABSOLUTE_DIR', 'userfiles/images/');
-?>
